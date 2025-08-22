accd2d57728e266777cfda026220c848f1c61edf
Guilherme Abtibol <guiabtibol@gmail.com>
2025-08-22 13:53:29 -0400

    Adicionando o resto dos exercicios dessa aula

---
+++ codigos_python/21_08/resto.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite o primeiro numero: "))
+n2 = float(input("Digite o segundo numero: "))
+
+if n2 != 0 :
+    resultado = n1 & n2
+    print("O resultado da divisao é: ", resultado)
+else :
+    print("Nao pode ser por zero")
